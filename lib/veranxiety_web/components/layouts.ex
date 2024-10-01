398261b7ccd9b85498ac1c255141c05234738d8e
Georg Ekeberg <georghagen@gmail.com>
2024-10-01 20:59:08 +0200

    added back session new edit code again...

--- lib/veranxiety_web/components/layouts.ex
+++ lib/veranxiety_web/components/layouts.ex
@@ -7,12 +7,12 @@ defmodule VeranxietyWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <div class="flex flex-col h-screen dark:text-rose bg-base dark:bg-base-dark">
-      <header class="flex-none bg-surface dark:bg-surface-dark shadow z-10 sticky top-0">
+    <div class="flex flex-col h-screen overflow-hidden dark:text-rose bg-base dark:bg-base-dark">
+      <header class="flex-none bg-surface dark:bg-surface-dark md:shadow z-10">
         <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between items-center">
             <div class="flex items-center space-x-4">
-              <DarkLightToggle.dark_light_toggle />
+            <DarkLightToggle.dark_light_toggle />
               <a href="/" class="text-2xl font-bold text-text dark:text-text-dark">
                 Veranxiety
               </a>
@@ -23,14 +23,12 @@ defmodule VeranxietyWeb.Layouts do
       </header>
 
       <main class="flex-1 overflow-y-auto">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div class="max-w-7xl mt-4 mx-0 px-2 sm:mx-auto py-0 md:py-6 sm:px-6 lg:px-8">
           <%= @inner_content %>
         </div>
       </main>
-
       <BottomNav.bottom_nav />
-
-      <footer class="flex-none bg-surface dark:bg-surface-dark py-4 text-center text-sm text-gray-500 dark:text-gray-400">
+      <footer class="bg-surface dark:bg-surface-dark py-4 text-center text-sm text-gray-500 dark:text-gray-400">
         <p>&copy; <%= DateTime.utc_now().year %> Veranxiety. All rights reserved.</p>
       </footer>
     </div>
